da550f49c7c688e9c0769695572da9df218ce8c2
Sebastien Vermeille <sebastien.vermeille@gmail.com>
2024-08-20 12:50:41 +0200

    Cleanup

---
+++ stylesniffer-api/src/main/java/dev/cookiecode/stylesniffer/api/exception/StyleSnifferException.java
@@ -0,0 +1,7 @@
+package dev.cookiecode.stylesniffer.api.exception;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class StyleSnifferException extends RuntimeException {
+}
